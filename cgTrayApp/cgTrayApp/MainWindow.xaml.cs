e0dd8987c720984bbd4d66fa5a939419206252e4
Andreas Hove <andreas.h@live.no>
2016-12-28 22:49:46 +0100

    experimenting with tray icons and popups

--- cgTrayApp/cgTrayApp/MainWindow.xaml.cs
+++ cgTrayApp/cgTrayApp/MainWindow.xaml.cs
@@ -1,30 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace cgTrayApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
+
+
     public partial class MainWindow : Window
     {
+        /*
+        private TaskbarIcon tb;
 
+        private void InitApplication()
+        {
+            //initialize NotifyIcon
+            tb = (TaskbarIcon)FindResource("MyNotifyIcon");
+        }*/
         public MainWindow()
         {
+            
             InitializeComponent();
+            
+            
         }
-      
+        /*
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+            this.nIcon.Icon = new Icon(@"../../st_le_.ico");
+            this.nIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
+            this.nIcon.Visible = true;
+        }
+        */
     }
 }
