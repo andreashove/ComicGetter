bd4e96efa025156d1c6ec0c179f9360451d83de5
Andreas Hove <andreas.h@live.no>
2017-01-03 23:57:17 +0100

    copying files to fav folder when pushin save button

--- cgTrayApp/cgTrayApp/MainWindow.xaml.cs
+++ cgTrayApp/cgTrayApp/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
-﻿
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Forms;
 using System.Drawing;
 using System;
 using Hardcodet.Wpf.TaskbarNotification;
+using System.IO;
 
 namespace cgTrayApp
 {
@@ -28,18 +28,55 @@ namespace cgTrayApp
         {
             
             InitializeComponent();
+            MoveCurrentImageToFavs();
+            Environment.Exit(0);
             
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MoveCurrentImageToFavs();
+        }
+        
+        
+        void MoveCurrentImageToFavs()
+        {
+            string sourcePath = "c:\\ComicGetter\\img";
+            string targetPath = "c:\\ComicGetter\\favs";
+            string fileToCopy = "comicstrip.jpg";
+
+            // count # of images in favs folder
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(targetPath);
+            int count = dir.GetFiles().Length;
+
+            System.Windows.MessageBox.Show(count.ToString());
+            int nextFavFileId = count+1;
+            string newFileName = nextFavFileId.ToString();
+            newFileName += ".gif";
+
+
+            string sourceFile;
+            string targetFile;
+            //System.Windows.MessageBox.Show("clicked!");
+            
+            sourceFile = Path.Combine(sourcePath, fileToCopy);
+            targetFile = Path.Combine(targetPath, newFileName);
+
+            File.Copy(sourceFile, targetFile, true);
+           
             
         }
         /*
+
         
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = System.Windows.WindowState.Minimized;
-            this.nIcon.Icon = new Icon(@"../../st_le_.ico");
-            this.nIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
-            this.nIcon.Visible = true;
-        }
-        */
+private void Button_Click(object sender, RoutedEventArgs e)
+{
+   this.WindowState = System.Windows.WindowState.Minimized;
+   this.nIcon.Icon = new Icon(@"../../st_le_.ico");
+   this.nIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
+   this.nIcon.Visible = true;
+}
+*/
     }
 }
